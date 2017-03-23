41a24e65494f0617b1b361eb1531256bcab02f3f
rubenmartin.thys <rubenmartin.thys@student.odisee.be>
2017-03-23 15:35:30 +0100

    basic notification shoudl work now

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
@@ -2,12 +2,31 @@ package com.team_htbr.a1617proj1bloeddonatie_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		Button btnShowToken = (Button)findViewById(R.id.button_show_token);
+		btnShowToken.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Get the token
+				String token = FirebaseInstanceId.getInstance().getToken();
+				Log.d(TAG, "Token: " + token);
+				Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+			}
+		});
     }
 }
