9a122da036d4c71796265ef12e5edb83fff7e01f
Bjorn Vandewalle <Bjorn Vandewalle>
2017-03-29 13:13:06 +0200

    layout fix

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/SubscribeBloodtypeActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/SubscribeBloodtypeActivity.java
@@ -2,6 +2,10 @@ package com.team_htbr.a1617proj1bloeddonatie_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class SubscribeBloodtypeActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class SubscribeBloodtypeActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_subscribe_bloodtype);
+		setTitle("Kies bloed type");
+	}
+
+	private void Unsubscribe() {
+		FirebaseMessaging.getInstance().unsubscribeFromTopic("blood-A");
+		FirebaseMessaging.getInstance().unsubscribeFromTopic("blood-B");
+		FirebaseMessaging.getInstance().unsubscribeFromTopic("blood-AB");
+		FirebaseMessaging.getInstance().unsubscribeFromTopic("blood-O");
 	}
 }
