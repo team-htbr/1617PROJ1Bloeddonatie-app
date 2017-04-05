e512040b2b235257ce6517b406e96f1b7881a6cd
Bjorn Vandewalle <Bjorn Vandewalle>
2017-04-05 14:28:38 +0200

    desgign improvements

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/DonorTestActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/DonorTestActivity.java
@@ -9,5 +9,6 @@ public class DonorTestActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_donor_test);
+		setTitle("Donor Test");
 	}
 }
