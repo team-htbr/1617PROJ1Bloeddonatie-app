056ba60925d99a751a4aa3bba1be614b5ed8a0a7
Ruben Thys <ruben.thys@student.odisee.be>
2017-04-24 19:12:34 +0200

    remove unused imports

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
@@ -8,19 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
