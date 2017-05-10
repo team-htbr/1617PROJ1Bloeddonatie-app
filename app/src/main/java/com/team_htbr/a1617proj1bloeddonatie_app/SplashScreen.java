279e73a84784d5f488b1f358e83d356f22b9291c
TCamm <thibautcammaert@hotmail.com>
2017-05-10 15:33:48 +0200

    removed unused import

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/SplashScreen.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/SplashScreen.java
@@ -2,7 +2,6 @@ package com.team_htbr.a1617proj1bloeddonatie_app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends Activity {
