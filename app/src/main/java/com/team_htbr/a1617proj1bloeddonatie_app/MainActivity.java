7f94c7cd6c2fec9d8aeaf6c1cf674c510c9ae01c
Thibaut Cammaert <thibaut.cammaert@student.odisee.be>
2017-05-17 11:52:37 +0200

    side bar fully functioning

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
@@ -1,9 +1,13 @@
 package com.team_htbr.a1617proj1bloeddonatie_app;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,6 +17,8 @@ import com.google.firebase.iid.FirebaseInstanceId;
 public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = "MainActivity";
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mToggle;
 
 
     @Override
@@ -20,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setTitle("Rode Kruis");
-		
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+		mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+		mDrawerLayout.addDrawerListener(mToggle);
+		mToggle.syncState();
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
 		Button btnBloodtype = (Button) findViewById(R.id.Bloodtype);
 		btnBloodtype.setOnClickListener(new View.OnClickListener() {
@@ -49,5 +63,13 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(new Intent(MainActivity.this, MapsActivity.class));
 			}
 		});
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(mToggle.onOptionsItemSelected(item)){
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
