440325588cb09410cad27d2d3f2fbcdebda18ac9
Bjorn Vandewalle <bjorntje_0204@hotmail.com>
2017-06-11 16:24:03 +0200

    improvment

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
@@ -49,16 +49,42 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		setTitle("Rode Kruis");
 
-		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1234);
-		}
-
 		geofences = new ArrayList<>();
 		locationsList = new ArrayList<>();
 		locationKeys = loadList();
 
 		locationKeys.clear();
 
+		requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION}, 1234);
+
+		connectToGoogleApi();
+
+		Button btnBloodtype = (Button) findViewById(R.id.Bloodtype);
+		btnBloodtype.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, SubscribeBloodtypeActivity.class));
+			}
+		});
+
+		Button btnDonorTest = (Button) findViewById(R.id.donorTest);
+		btnDonorTest.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, DonorTestActivity.class));
+			}
+		});
+
+		Button btnMaps = (Button) findViewById(R.id.GoogleMap);
+		btnMaps.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, MapsActivity.class));
+			}
+		});
+	}
+
+	private void checkDataBase() {
 		DatabaseReference fireBaseDataBase = FirebaseDatabase.getInstance().getReference();
 		DatabaseReference locationsDataBase = fireBaseDataBase.child("locations");
 
@@ -95,35 +121,6 @@ public class MainActivity extends AppCompatActivity {
 				Log.d(TAG, "bla bla");
 			}
 		});
-
-
-
-		Button btnBloodtype = (Button) findViewById(R.id.Bloodtype);
-		btnBloodtype.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(MainActivity.this, SubscribeBloodtypeActivity.class));
-			}
-		});
-
-		Button btnDonorTest = (Button) findViewById(R.id.donorTest);
-		btnDonorTest.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(MainActivity.this, DonorTestActivity.class));
-			}
-		});
-
-		Button btnMaps = (Button) findViewById(R.id.GoogleMap);
-		btnMaps.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(MainActivity.this, MapsActivity.class));
-			}
-		});
-
-		connectToGoogleApi();
-		startLocationMoitoring();
 	}
 
 	private void connectToGoogleApi() {
@@ -188,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
 					.setRequestId(location.getName())
 					.setCircularRegion(location.getLat(), location.getLng(), 20000)
 					.setExpirationDuration(Geofence.NEVER_EXPIRE)
-					.setNotificationResponsiveness(5000000)
+					.setNotificationResponsiveness(5000)
 					.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
 					.build());
 			}
@@ -256,6 +253,20 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		return loadedList;
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case 1234: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					startLocationMoitoring();
+					checkDataBase();
+				} else {
+					return;
+				}
+			}
+		}
 	}
 }
 
