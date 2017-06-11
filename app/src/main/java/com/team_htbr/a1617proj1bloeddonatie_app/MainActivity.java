7c641e62c50c8da97be64f64b5447b669902cb8e
Thibaut Cammaert <thibaut.cammaert@student.odisee.be>
2017-06-11 17:26:12 +0200

    removed unused import

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MainActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
