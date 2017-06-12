37ebb5ca928889b1b338d93c502462d1e6f554d3
Bjorn Vandewalle <bjorntje_0204@hotmail.com>
2017-06-12 21:01:12 +0200

    delete unessesairy comments

--- app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MapsActivity.java
+++ app/src/main/java/com/team_htbr/a1617proj1bloeddonatie_app/MapsActivity.java
@@ -154,12 +154,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			return true;
 		}
